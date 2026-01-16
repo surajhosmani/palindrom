ed6efb88ab18767cf332dfa4f906cf3fbbe23e44
surajhosmani <surajhosmani83@gmail.com>
2026-01-16 12:45:55 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,10 @@
 def check_palindrome(text):
-    if text == text[::-1]:
-        return "Palindrome"
-    else:
-        return "Not Palindrome"
+    return text == text[::-1]
+
 
 if __name__ == "__main__":
     text = input("Enter a string: ")
-    print(
-        "The string is a Palindrome"
-        if check_palindrome(text) == "Palindrome"
-        else "The string is not a Palindrome"
-    )
+    if check_palindrome(text):
+        print("The string is a Palindrome")
+    else:
+        print("The string is not a Palindrome")
